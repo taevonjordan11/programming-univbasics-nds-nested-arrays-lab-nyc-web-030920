f8a2bba8329973db0fd44b17ce281c83ebaf92ea
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-28 20:06:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -89,7 +89,8 @@ def matrix_lookup(matrix, row, column)
 ]
     ]
     
-   puts storage_room = [conventional_produce.sort, organic_produce.sort]
+   storage_room = [conventional_produce.sort, organic_produce.sort]
+   storage_room[1][3]
     
     
     
