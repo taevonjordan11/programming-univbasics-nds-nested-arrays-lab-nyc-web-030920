392898764159579f8db1528e74a3df21627994f3
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-28 20:45:56 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -104,5 +104,7 @@ def matrix_update(matrix, row, column, new_value)
   "Asparagus"
 ]
     ] 
-    matrix[0][2] = "Lemons"
+    conventional_produce[0][2] = "Lemons"
+    return conventional_produce
+    
 end
